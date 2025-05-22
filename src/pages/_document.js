80fbce419ff4693ffa7813baa4d84fc925be1942
Innis <1575270674@qq.com>
2025-05-22 09:42:13 +0800

    init commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>The Blessing Asia</title>
+        <link rel="icon" href="/cyc-logo.png" type="image/png" sizes="32x32" />
+      </Head>
+
       <body className="antialiased">
         <Main />
         <NextScript />
