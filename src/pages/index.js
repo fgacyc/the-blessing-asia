ff0c67f823924cf2a20266cf7c84fff83fcb975b
Innis <1575270674@qq.com>
2025-05-22 10:01:18 +0800

    feat: add title

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,9 @@ export default function Home() {
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl sm:text-6xl font-bold tracking-[-.01em] text-center sm:text-left">
+          Welcome to <span className="">The Blessing Asia</span>
+        </h1>
         <Image
           className="dark:invert"
           src="/next.svg"
